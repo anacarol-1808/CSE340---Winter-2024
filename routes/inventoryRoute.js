35f177cc874a5d90ac695f91e35e53e7b27ac882
anacarol-1808 <anacarol_1808@hotmail.com>
2024-02-02 00:24:59 -0700

    Creating item detail view

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,8 +2,12 @@
 const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
+const itemDetailController = require("../controllers/itemDetailController")
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
+
+// Route to deliver a specific inventory item detail view
+router.get("/detail/:vehicleId", itemDetailController.showItemDetail);
 
 module.exports = router;
