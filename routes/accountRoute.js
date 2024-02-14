e95972547fc2fba6302382892a8c4b635ba8ff58
anacarol-1808 <anacarol_1808@hotmail.com>
2024-02-13 22:36:14 -0700

    Datavalidataion activity complete

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,11 +3,17 @@ const express = require("express")
 const router = new express.Router()
 const utilities = require("../utilities/index")
 const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
 
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
 // Enable Registration Route
-router.post('/register', utilities.handleErrors(accountController.registerAccount));
+router.post(
+    '/register',
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+);
 
 module.exports = router;
